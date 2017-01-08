1a60598a0fce8c0c4a1f4aabfecb4dd74c8c3385
Narongsak kongpan <disengage.nrskp@gmail.com>
2017-01-08 22:50:18 +0700

    Initial Commit

---
+++ NotifyMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotifyMe
+//
+//  Created by Narongsak kongpan on 1/8/2560 BE.
+//  Copyright © 2560 Narongsak kongpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
